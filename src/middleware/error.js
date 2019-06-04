9f045983b5a53a9bdb47c6421469054a9b5315ce
Frazer Smith <frazer.smith@ydh.nhs.uk>
2019-06-04 12:25:48 +0100

    Lint middleware

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,5 +1,5 @@
-module.exports = function(){
-	return function(err, req, res, next){
+module.exports = function () {
+	return function (err, req, res, next) {
 		if (res.headersSent) {
 			return next(err);
 		}
