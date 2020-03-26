a0ec8d67be35eb84d4369f2abd4d0837461677f3
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-03-26 11:39:16 +0000

    refactor(server): use nocache module as helmet.nocache is deprecated

--- src/server/server.js
+++ src/server/server.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const helmet = require('helmet');
 const http = require('http');
 const https = require('https');
+const nocache = require('nocache');
 const passport = require('passport');
 const { Strategy } = require('passport-http-bearer');
 const winston = require('winston');
@@ -93,7 +94,7 @@ class Server {
 	configureHelmet(helmetConfig) {
 		// Use Helmet to set response headers
 		this.app.use(helmet(helmetConfig));
-		this.app.use(helmet.noCache());
+		this.app.use(nocache());
 
 		// Return self for chaining
 		return this;
