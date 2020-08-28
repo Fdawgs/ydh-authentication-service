32e3349e6cd2474e9df7685f5d9f5fd02e75f0d1
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-08-28 13:59:52 +0100

    chore(pm2): increase cluster instances from 8 to 16

--- .pm2.config.js
+++ .pm2.config.js
@@ -7,7 +7,7 @@ module.exports = {
 				NODE_ENV: 'production'
 			},
 			exec_mode: 'cluster',
-			instances: 8,
+			instances: 16,
 			name: 'auth-serv',
 			script: './src/index.js',
 			watch: ['./src/config.js', '.env.production']
