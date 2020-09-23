66b5a63bd08a53b71dd0d6fa02de7fcf78d6ee26
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-09-23 13:55:13 +0100

    feat(utils): add openid connect jwt verification support

--- src/server/utils/bearer-token-auth.utils.js
+++ src/server/utils/bearer-token-auth.utils.js
@@ -1,12 +1,42 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-else-return */
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+/**
+ * @author Mark Hunt
+ * @author Frazer Smith
+ * @param {string} token
+ * @returns {Promise}
+ */
+async function getSigningKey(token) {
+	return new Promise((resolve, reject) => {
+		const client = jwksClient({
+			strictSsl: true, // Default value
+			jwksUri: process.env.OPENID_JWKS_ENDPOINT
+		});
+		const decoded = jwt.decode(token, { complete: true });
+		client.getSigningKey(decoded.header.kid, (err, key) => {
+			if (err) {
+				reject(err);
+			} else {
+				const signingKey = key.publicKey || key.rsaPublicKey;
+				resolve(signingKey);
+			}
+		});
+	});
+}
+
 /**
  * @author Frazer Smith
- * @description Checks 'authorization' request header value against array of API key objects.
+ * @description Checks 'authorization' request header value against array
+ * of API key objects or OpenID Connect.
  * @param {string} token - Bearer token.
  * @param {Function} callback - PassportJS strategy callback.
  * @param {Array.<{value: string}>} authArray - Array of API key objects.
  * @returns {Function} callback.
  */
-module.exports = function bearerTokenAuthentication(
+module.exports = async function bearerTokenAuthentication(
 	token,
 	callback,
 	authArray
@@ -18,6 +48,25 @@ module.exports = function bearerTokenAuthentication(
 
 	if (keys.includes(token)) {
 		return callback(null, token);
+	} else if (process.env.OPENID_JWKS_ENDPOINT) {
+		const signingKey = await getSigningKey(token);
+		jwt.verify(
+			token,
+			signingKey,
+			{
+				audience: process.env.JWT_ALLOWED_AUDIENCE,
+				algorithms: process.env.JWT_ALLOWED_ALGO_ARRAY,
+				ignoreExpiration: false,
+				maxAge: process.env.JWT_MAX_AGE
+			},
+			(err, decoded) => {
+				if (err) {
+					return callback(null, false);
+				}
+				return callback(null, token);
+			}
+		);
+	} else {
+		return callback(null, false);
 	}
-	return callback(null, false);
 };
