11360821b469ed9317a5c570083e9469b99a37c9
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-11-24 12:35:36 +0000

    fix(routes): protect all request types with bearer token authentication

--- src/server/routes/wildcard.route.js
+++ src/server/routes/wildcard.route.js
@@ -18,44 +18,43 @@ const sanitize = require('sanitize-middleware');
 module.exports = function wildcardRoute(options) {
 	const config = options;
 
-	router.use(sanitize(), cors(config.cors));
+	router.use(
+		sanitize(),
+		passport.authenticate('bearer', { session: false }),
+		cors(config.cors)
+	);
 
-	router
-		.route('*')
-		.get(
-			passport.authenticate('bearer', { session: false }),
-			async (req, res, next) => {
-				try {
-					const response = await request.get(
-						`${
-							config.listenerUrl + req.baseUrl
-						}?${queryString.stringify(req.query)}`,
-						{
-							responseType: 'stream'
-						}
-					);
-
-					// Remove CORS headers
-					delete response.headers['access-control-allow-methods'];
-					delete response.headers['access-control-allow-headers'];
-					delete response.headers['access-control-expose-headers'];
-
-					// Remove or amend inaccurate headers
-					delete response.headers.etag;
-					delete response.headers['last-modified'];
-
-					// Remove security risk headers
-					delete response.headers.location;
-					delete response.headers.server;
-
-					res.set(response.headers);
-					response.data.pipe(res);
-				} catch (err) {
-					res.status(500);
-					next(new Error(err));
+	router.route('*').get(async (req, res, next) => {
+		try {
+			const response = await request.get(
+				`${config.listenerUrl + req.baseUrl}?${queryString.stringify(
+					req.query
+				)}`,
+				{
+					responseType: 'stream'
 				}
-			}
-		);
+			);
+
+			// Remove CORS headers
+			delete response.headers['access-control-allow-methods'];
+			delete response.headers['access-control-allow-headers'];
+			delete response.headers['access-control-expose-headers'];
+
+			// Remove or amend inaccurate headers
+			delete response.headers.etag;
+			delete response.headers['last-modified'];
+
+			// Remove security risk headers
+			delete response.headers.location;
+			delete response.headers.server;
+
+			res.set(response.headers);
+			response.data.pipe(res);
+		} catch (err) {
+			res.status(500);
+			next(new Error(err));
+		}
+	});
 
 	return router;
 };
